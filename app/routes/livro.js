34b7ead3575a109d4ed20f3dda78c3ce2e43ba34
haganicolau <haganicolau@gmail.com>
2016-09-23 00:43:26 -0300

    Últimas alterações

--- app/routes/livro.js
+++ app/routes/livro.js
@@ -1,13 +1,18 @@
 module.exports = function(app){
   var controller = app.controllers.livro;
 
-  app.route('/livros')
+  app.route('/v1/livros')
     .post(controller.create)
-    .get(controller.findAll)
+    .get(controller.findAll);
+    
+
+  app.route("/v1/livros/:id")
+    .get(controller.findOne)
+    .delete(controller.delete)
     .put(controller.update);
 
-  app.route("/livros/:id")
-    .get(controller.findOne)
-    .delete(controller.delete);
+  app.all('/*', function(req, res){
+    res.sendFile(path.resolve('public/index.html')); 
+  });
 
 }
--- app/routes/usuario.js
+++ app/routes/usuario.js
@@ -4,12 +4,12 @@ module.exports = function (app) {
     // módulo de controller que contém nossas regras de negócio.
     var controller = app.controllers.usuario;
 
-    app.route('/usuarios')
+    app.route('/v1/usuarios')
         .get(controller.findAll)
         .post(controller.create)
         .put(controller.update);
 
-    app.route('/usuarios/:id')
+    app.route('/v1/usuarios/:id')
         .get(controller.findOne)
         .delete(controller.delete);
 
